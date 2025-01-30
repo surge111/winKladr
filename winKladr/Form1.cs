5544c4d5249b00e1c773b3b2ceb2e6d8ae88ad3d
userzamt <zamt.it@yandex.ru>
2025-01-30 10:44:58 +0300

    Sqlite. Настройка проекат под x64. База КЛАДР

--- winKladr/Form1.cs
+++ winKladr/Form1.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SQLite;
+using System.IO;
+
+// Sqlite dll взял на платформу x64, по этому необходимо настроить проект
+// Project Properties -> Build -> Platform target -> x64
+
 namespace winKladr
 {
     public partial class Form1 : Form
@@ -15,6 +21,15 @@ namespace winKladr
         public Form1()
         {
             InitializeComponent();
+
+            string pathDb = Directory.GetCurrentDirectory() + @"\db\kladr.db";
+            string connectionStr = $@"Data Source={pathDb};Version=3;Read Only=True;";
+
+            SQLiteConnection con = new SQLiteConnection(connectionStr);
+            con.Open();
+
+
+            con.Close();
         }
     }
 }
